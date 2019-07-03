c52bd3871a6e3677b2413b439626b5f00ce12d3f
Matt Henley <mattbhenley@gmail.com>
2019-07-03 13:27:58 -0500

    Initial commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Matt Henley on 7/3/19.
+//  Copyright © 2019 Matt Henley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
