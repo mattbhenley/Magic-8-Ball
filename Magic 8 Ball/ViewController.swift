dddb240bca93453484ae968278ac62ebaa13fcb0
Matt Henley <mattbhenley@gmail.com>
2019-07-03 14:10:34 -0500

    added array, random function and linked images

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let ballArray = ["ball1","ball2", "ball3", "ball4", "ball5"]
+    
+    var randomBallNumber: Int = 0
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func askButtonPressed(_ sender: Any) {
+        newBallImage()
+        
+    }
+    
+    func newBallImage() {
+        randomBallNumber = Int.random(in: 0 ... 4 )
+        
+        print(randomBallNumber)
+        
+        imageView.image = UIImage(named:ballArray[randomBallNumber])
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
