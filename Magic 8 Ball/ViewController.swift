a45fe42427e49809152c2e163e4c0d7bba0fa891
Matt Henley <mattbhenley@gmail.com>
2019-07-03 14:19:29 -0500

    added startup code and comments

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -36,8 +36,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print(randomBallNumber)
+        // presents random image on startup
+        print(newBallImage())
     }
 
 
