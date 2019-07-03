6160a5d0aef338702023cc6122dc9cd8bf4f2ede
Matt Henley <mattbhenley@gmail.com>
2019-07-03 14:17:17 -0500

    added gesture and adding image on startup

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -37,7 +37,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        print(randomBallNumber)
     }
 
 
